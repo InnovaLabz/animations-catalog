cdfdab39cbba33e878ac2cbc9063b24e5c5c4aa0
Andy Osei <anyosei36@gmail.com>
2021-09-06 00:20:11 +0000

    progress value digits sliding in and out

--- lib/download_progress/page.dart
+++ lib/download_progress/page.dart
@@ -2,8 +2,38 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-class DownloadProgressPage extends StatelessWidget {
+final increments = [5, 10, 8, 12];
+
+class DownloadProgressPage extends StatefulWidget {
   const DownloadProgressPage({Key? key}) : super(key: key);
+
+  @override
+  _DownloadProgressPageState createState() => _DownloadProgressPageState();
+}
+
+class _DownloadProgressPageState extends State<DownloadProgressPage> {
+  double _progress = 0;
+  Timer? _timer;
+  bool _startDownload = false;
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(milliseconds: 500), (timer) {
+      setState(() {
+        final index = _progress % increments.length;
+        _progress += increments[index.toInt()];
+        if (_progress >= 100) {
+          _progress = 100;
+          _timer?.cancel();
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +49,40 @@ class DownloadProgressPage extends StatelessWidget {
             DownloadProgress(
               width: size.width * 0.7,
               height: 60,
+              progress: _progress,
+              start: _startDownload,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      if (!_startDownload) {
+                        _startDownload = true;
+                        _startTimer();
+                      }
+                    });
+                  },
+                  child: const Text(
+                    'Animate',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _startDownload = false;
+                      _progress = 0;
+                    });
+                  },
+                  child: const Text(
+                    'Reset',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
@@ -27,17 +90,19 @@ class DownloadProgressPage extends StatelessWidget {
   }
 }
 
-final increments = [5, 10, 8, 12];
-
 class DownloadProgress extends StatefulWidget {
   const DownloadProgress({
     Key? key,
     required this.width,
     required this.height,
+    required this.start,
+    this.progress = 0,
   }) : super(key: key);
 
   final double width;
   final double height;
+  final bool start;
+  final double progress;
 
   @override
   _DownloadProgressState createState() => _DownloadProgressState();
@@ -45,14 +110,11 @@ class DownloadProgress extends StatefulWidget {
 
 class _DownloadProgressState extends State<DownloadProgress>
     with SingleTickerProviderStateMixin {
-  double overlayWidthFactor = 0;
-  double _widthFactor = 0;
-  double _progress = 0;
-  Timer? _timer;
   late AnimationController _controller;
   late Animation<Offset> _downloadTextSlideIn;
   late Animation<RelativeRect> _expandRightAnimation;
-  late Animation<RelativeRect> _downloadTextSlideIn2;
+
+  bool _showProgressIndicator = false;
 
   @override
   void initState() {
@@ -60,35 +122,31 @@ class _DownloadProgressState extends State<DownloadProgress>
 
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 900),
+      duration: const Duration(milliseconds: 700),
     )..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          _timer = _startTimer();
+          setState(() {
+            _showProgressIndicator = true;
+          });
+        } else {
+          setState(() {
+            _showProgressIndicator = false;
+          });
         }
       });
 
     _downloadTextSlideIn =
-        Tween<Offset>(begin: const Offset(0, 1), end: const Offset(0, 0))
+        Tween<Offset>(begin: const Offset(0, 1.2), end: const Offset(0, 0))
             .animate(
       CurvedAnimation(
         parent: _controller,
         curve: const Interval(
           0.000,
-          0.400,
-          curve: Curves.easeOut,
+          0.300,
+          curve: Curves.ease,
         ),
       ),
     );
-
-    // _downloadTextSlideIn2 = RelativeRectTween(
-    //   begin: RelativeRect.fromSize(Rect.fromLTWH(left, top, width, height), container),
-    //   end: const RelativeRect.fromLTRB(100.0, 0, 0, 0),
-    // ).animate(
-    //   CurvedAnimation(
-    //     parent: _controller,
-    //     curve: const Interval(0.000, 0.400, curve: Curves.easeOut),
-    //   ),
-    // );
 
     _expandRightAnimation = RelativeRectTween(
       begin: RelativeRect.fromLTRB(widget.width, 0, 0, 0.0),
@@ -100,93 +158,335 @@ class _DownloadProgressState extends State<DownloadProgress>
       ),
     );
 
-    Future.delayed(const Duration(seconds: 1), () {
-      _controller.forward();
-    });
-  }
-
-  Timer _startTimer() {
-    return Timer.periodic(const Duration(milliseconds: 500), (timer) {
-      setState(() {
-        final index = _progress % increments.length;
-        _progress += increments[index.toInt()];
-        _widthFactor = _progress / 100.0;
-        if (_progress >= 100) {
-          _progress = 100;
-          _widthFactor = 1.0;
-          _timer?.cancel();
-        }
-      });
+    Future.delayed(Duration.zero, () {
+      if (widget.start) {
+        _controller.forward();
+      }
     });
   }
 
   @override
+  void didUpdateWidget(DownloadProgress oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.start != widget.start) {
+      if (widget.start == true) {
+        if (_controller.isAnimating) {
+          _controller.reset();
+          _controller.forward();
+        } else {
+          _controller.forward();
+        }
+      } else {
+        _showProgressIndicator = false;
+        _controller.reset();
+      }
+    }
+  }
+
+  @override
   void dispose() {
-    _timer?.cancel();
+    _controller.dispose();
     super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Stack(
+        children: [
+          Container(
+            width: widget.width,
+            height: widget.height,
+            color: const Color(0xFF9A2BED),
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Positioned(
+                  right: 20,
+                  bottom: widget.height * 0.3,
+                  child: SlideInText(
+                    animation: _downloadTextSlideIn,
+                    text: 'DOWNLOAD',
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    height: widget.height * 0.3,
+                    color: const Color(0xFF9A2BED),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          PositionedTransition(
+            rect: _expandRightAnimation,
+            child: Container(
+              color: const Color(0xFF6922BB),
+            ),
+          ),
+          if (_showProgressIndicator) ...[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: ProgressIndicator(
+                progress: widget.progress,
+                width: widget.width,
+                height: widget.height,
+              ),
+            ),
+          ]
+        ],
+      ),
+    );
+  }
+}
+
+// class ProgressIndicator extends StatelessWidget {
+//   const ProgressIndicator({
+//     Key? key,
+//     required this.width,
+//     required this.height,
+//   }) : super(key: key);
+
+//   final double width;
+//   final double height;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AnimatedContainer(
+//       duration: const Duration(milliseconds: 200),
+//       curve: Curves.easeOut,
+//       width: width,
+//       height: height,
+//       color: Colors.white,
+//     );
+//   }
+// }
+
+class SlideInText extends AnimatedWidget {
+  const SlideInText({
+    Key? key,
+    required this.animation,
+    required this.text,
+  }) : super(key: key, listenable: animation);
+
+  final Animation<Offset> animation;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: animation,
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class ProgressIndicator extends StatefulWidget {
+  const ProgressIndicator({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.progress,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+  final double progress;
+
+  @override
+  _ProgressIndicatorState createState() => _ProgressIndicatorState();
+}
+
+class _ProgressIndicatorState extends State<ProgressIndicator> {
+  List<Widget> _buildProgressDigits() {
+    final widgetList = <Widget>[];
+    final progressValueDigits = widget.progress.toInt().toString().split('');
+    for (int i = 0; i < progressValueDigits.length; i++) {
+      final digit = progressValueDigits[i];
+      widgetList.add(
+        ProgressValueDigit(
+          width: widget.width,
+          height: widget.height,
+          progressDigit: int.parse(digit),
+        ),
+      );
+    }
+    return widgetList;
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          width: widget.width,
-          height: widget.height,
-          color: const Color(0xFF9A2BED),
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              // Align(
-              //   alignment: Alignment.centerRight,
-              //   child: SlideTransition(
-              //     position: _downloadTextSlideIn,
-              //     child: const Text(
-              //       'DOWNLOAD',
-              //       style: TextStyle(color: Colors.white),
-              //     ),
-              //   ),
-              // ),
-              PositionedTransition(
-                rect: _downloadTextSlideIn2,
-                child: const Text(
-                  'DOWNLOAD',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              Positioned(
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: Container(
-                  height: widget.height * 0.3,
-                  color: const Color(0xFF9A2BED),
-                ),
-              ),
-              Positioned(
-                left: 0,
-                right: 0,
-                top: 0,
-                child: Container(
-                  height: widget.height * 0.3,
-                  color: const Color(0xFF9A2BED),
-                ),
-              ),
-            ],
-          ),
-        ),
-        PositionedTransition(
-          rect: _expandRightAnimation,
-          child: Container(
-            color: const Color(0xFF6922BB),
-          ),
-        ),
         AnimatedContainer(
-          duration: const Duration(milliseconds: 200),
+          duration: const Duration(milliseconds: 300),
           curve: Curves.easeOut,
-          width: _widthFactor * widget.width,
+          width: (widget.progress / 100) * widget.width,
           height: widget.height,
           color: Colors.white,
+        ),
+        Positioned(
+          left: 12,
+          top: widget.height * 0.3,
+          bottom: widget.height * 0.3,
+          child: SizedBox(
+            width: widget.width,
+            height: widget.height,
+            child: Row(
+              children: [
+                ..._buildProgressDigits(),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          top: 0,
+          child: Container(
+            height: widget.height * 0.2,
+            color: Colors.white,
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Container(
+            height: widget.height * 0.2,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ProgressValueDigit extends StatefulWidget {
+  const ProgressValueDigit({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.progressDigit,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+  final int progressDigit;
+
+  @override
+  _ProgressValueDigitState createState() => _ProgressValueDigitState();
+}
+
+class _ProgressValueDigitState extends State<ProgressValueDigit>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideOutPrevProgressDigit;
+  late Animation<Offset> _slideInPrevProgressDigit;
+  late Animation<double> _progressDigitOpacity;
+  late Animation<double> _prevProgressDigitOpacity;
+  late int _progressDigit;
+  int _prevProgressDigit = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _progressDigit = widget.progressDigit;
+
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 200));
+
+    _slideOutPrevProgressDigit = _controller
+        .drive(
+          CurveTween(curve: Curves.ease),
+        )
+        .drive(
+          Tween<Offset>(
+            begin: Offset.zero,
+            end: const Offset(0, -1),
+          ),
+        );
+
+    _prevProgressDigitOpacity = _controller
+        .drive(
+          CurveTween(curve: const Interval(0.300, 0.500, curve: Curves.ease)),
+        )
+        .drive(Tween<double>(begin: 1, end: 0));
+
+    _progressDigitOpacity = _controller
+        .drive(
+          CurveTween(curve: const Interval(0.500, 1.000, curve: Curves.ease)),
+        )
+        .drive(Tween<double>(begin: 0, end: 1.0));
+
+    _slideInPrevProgressDigit = _controller
+        .drive(
+          CurveTween(curve: Curves.ease),
+        )
+        .drive(
+          Tween<Offset>(
+            begin: const Offset(0, 2),
+            end: Offset.zero,
+          ),
+        );
+  }
+
+  @override
+  void didUpdateWidget(ProgressValueDigit oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.progressDigit != widget.progressDigit) {
+      _prevProgressDigit = oldWidget.progressDigit;
+      _progressDigit = widget.progressDigit;
+      _controller.reset();
+      _controller.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SlideTransition(
+          position: _slideOutPrevProgressDigit,
+          child: FadeTransition(
+            opacity: _prevProgressDigitOpacity,
+            child: Text(
+              _prevProgressDigit.toString(),
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        SlideTransition(
+          position: _slideInPrevProgressDigit,
+          child: FadeTransition(
+            opacity: _progressDigitOpacity,
+            child: Text(
+              _progressDigit.toString(),
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ],
     );
